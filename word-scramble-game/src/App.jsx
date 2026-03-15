1d3a8126ca8de03daf66f5db40bfa9d3ce94ca82
maayan3330 <maayan3330@gmail.com>
2026-03-15 15:23:17 +0200

    GameBoard

--- word-scramble-game/src/App.jsx
+++ word-scramble-game/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './components/Header';
+import GameBoard from './components/GameBoard';
 
 function App() {
   return (
+    <>
     <header>
       <Header/>
     </header>
+    <GameBoard/>זה לא עובד
+    </>
   );
 }
 export default App
---
+++ word-scramble-game/src/components/GameBoard.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import LetterBox from "./LetterBox";
+
+export default function GameBoard() {
+  // state for the letters the user will submit
+  const [word, setWord] = useState(["", "", "", "", "", ""]);
+
+  // fucntion to create the six components
+  function createBoxes() {
+    word.map((letter, index) => {
+      return (<LetterBox key={index} letter={letter} onChange={(event) => handleInput(index, event)}
+      />
+    );
+    });
+  }
+
+  // function to set input
+  function handleInput(index, event) {
+    const newWord = [...word];
+    newWord[index] = event.target.value;
+    setWord(newWord);
+  }
+
+  return <>{createBoxes()}</>;
+}
