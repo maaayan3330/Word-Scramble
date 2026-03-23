1c0843df2af5903ff10d307e81cb9983a20d3b79
Maayan Ifergan <maayan3330@gmail.com>
2026-03-24 00:17:31 +0200

    add key

--- word-scramble-game/src/components/GameBoard.jsx
+++ word-scramble-game/src/components/GameBoard.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState , useRef , useEffect} from "react";
 import LetterBox from "./LetterBox";
 
 export default function GameBoard() {
   // state for the letters the user will submit
   const [word, setWord] = useState(["", "", "", "", "", ""]);
+  const inputRefs = useRef([]);
 
   // fucntion to create the six components
    function createBoxes() {
@@ -13,21 +14,51 @@ export default function GameBoard() {
           key={index}
           letter={letter}
           onChange={(event) => handleInput(index, event)}
+          onKeyDown={(event) => handleKeyDown(index, event)}
+          inputRef={(element) => (inputRefs.current[index] = element)}
+          disabled={index !== 0 && word[index - 1] === ""}
         />
       );
     });
   }
+
+  // so the cursor will be on the first letter the moment the web up
+  useEffect(() => {
+    inputRefs.current[0]?.focus();
+  }, []);
 
   // function to set input
   function handleInput(index, event) {
     const newWord = [...word];
     newWord[index] = event.target.value;
     setWord(newWord);
+    // to move the focus to the next input
+    if (event.target.value !== "" && index < word.length - 1) {
+      setTimeout(() => {
+        inputRefs.current[index + 1]?.focus();
+      }, 0);
+    }
   }
 
+  function handleKeyDown(index, event) {
+    if (event.key === "Backspace" && word[index] === "" && index > 0) {
+      const newWord = [...word];
+      newWord[index - 1] = "";
+      setWord(newWord);
+
+      setTimeout(() => {
+        inputRefs.current[index - 1]?.focus();
+      }, 0);
+    }
+  }
+
+
   return (
-  <div className="flex justify-center items-center gap-4 mt-10">
-    {createBoxes()}
-  </div>
-);
+    <div
+      dir="ltr"
+      className="flex justify-center items-center gap-4 mt-10"
+    >
+      {createBoxes()}
+    </div>
+  );
 }
--- word-scramble-game/src/components/LetterBox.jsx
+++ word-scramble-game/src/components/LetterBox.jsx
@@ -1,11 +1,14 @@
-export default function LetterBox({ letter, onChange }) {
+export default function LetterBox({ letter, onChange ,onKeyDown, inputRef, disabled }) {
   return (
     <input
+      ref={inputRef}
       type="text"
       maxLength={1}
       value={letter}
       onChange={onChange}
-      className="w-14 h-14 text-center text-2xl font-bold border-2 border-slate-300 rounded-xl shadow-md focus:border-pink-400 outline-none"
+      onKeyDown={onKeyDown}
+      disabled={disabled}
+      className="w-30 h-30 text-center text-3xl font-bold border-2 border-slate-300 rounded-xl shadow-md focus:border-pink-400 outline-none"
     />
   );
 }
